ce6f0e0f6ac236c0cb6a0d881f0e5c587218a20f
RevathiRaj <30955765+RevathiRaj@users.noreply.github.com>
2017-08-16 18:31:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var img = document.getElementById('modi');
+img.onclick = function () {
+ img.margin.marginLeft = '100px'; 
+};
